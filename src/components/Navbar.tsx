8784932a9353f0d9e3653b04dfd56f8bafbbce01
Laerte Quinui <laertequinuio@gmail.com>
2025-07-03 12:17:31 -0300

    feat: Implement theme toggle

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,8 @@
+import { useEffect, useState } from 'react'
+import MoonIcon from '../assets/images/icon-moon.svg?react'
 import SunIcon from '../assets/images/icon-sun.svg?react'
 import Logo from '../assets/images/logo.svg?react'
+import '../styles/Navbar.css'
 
 const Navbar = () => {
   return (
@@ -11,9 +14,24 @@ const Navbar = () => {
 }
 
 const ThemeButton = () => {
+  const [theme, setTheme] = useState('dark')
+
+  const toggleTheme = () => {
+    setTheme(prevTheme => (prevTheme === 'light' ? 'dark' : 'light'))
+  }
+
+  localStorage.setItem('theme', 'dark')
+  useEffect(() => {
+    localStorage.setItem('theme', theme)
+
+    const storedTheme = localStorage.getItem('theme')
+    const body = document.getElementsByTagName('body')[0]
+    if (body) body.setAttribute('data-theme', storedTheme!)
+  }, [theme])
+
   return (
-    <button className='theme-button'>
-      <SunIcon />
+    <button className='theme-button' onClick={toggleTheme}>
+      {theme === 'dark' ? <SunIcon /> : <MoonIcon />}
 
       <div className='tooltip'>
         <span>Change theme</span>
